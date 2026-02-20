35c1f8fd0667f2254c74d413cd1116ef55c26fb8
dan <goveamartinezd@gmail.com>
2026-02-20 07:03:02 -0600

    PRIMER CAMBIO

---
+++ wii-demos/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', background: '#eeeeee' }}>
+      <h1>Demos cargando...</h1>
+    </div>
+  )
+}
+
+export default App
