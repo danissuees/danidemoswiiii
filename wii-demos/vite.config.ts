69b4b744323e1d7ba55358a3dc034f413ee04628
dan <goveamartinezd@gmail.com>
2026-03-12 12:59:27 -0600

    lol

--- wii-demos/vite.config.ts
+++ wii-demos/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // REEMPLAZA 'wii-demos' POR EL NOMBRE REAL DE TU REPOSITORIO EN GITHUB
-  base: '/wii-demos/', 
+  base: '/', 
 })
